a48057ca61086cc764f11ff1b93a88f64da7bac4
tim_jackins <jack@jacktimmins.com>
2020-02-17 15:34:59 -0500

    Updated db

--- db/migrate/20200109221647_create_encounters.rb
+++ db/migrate/20200109221647_create_encounters.rb
@@ -3,13 +3,12 @@ class CreateEncounters < ActiveRecord::Migration[6.0]
     create_table :encounters do |t|
       t.string :name
       t.integer :level
-      t.string :type
       t.string :reward
       t.text :description
 
       t.boolean :completed
 
-      # t.references :location, foreign_key: true
+      t.references :campaign, foreign_key: true
       # t.references :building, foreign_key: true
 
       t.timestamps
