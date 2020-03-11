6642894bb67f9df38684eef9055d8151270fb53a
tim_jackins <jack@jacktimmins.com>
2020-03-11 06:53:24 -0400

    Added react

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const coffee =  require('./loaders/coffee')
+// const coffee =  require('./loaders/coffee')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -8,5 +8,10 @@ environment.plugins.prepend('Provide',
   })
 )
 
-environment.loaders.prepend('coffee', coffee)
+// environment.loaders.prepend('coffee', coffee)
+environment.loaders.prepend('coffee', {
+  test: /\.coffee$/,
+  use: ['cjsx-loader', 'coffee-loader']
+})
+
 module.exports = environment
